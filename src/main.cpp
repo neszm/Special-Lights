ebe956eefe68802c9b127021f845a28e464bcd35
kamy-99 <kamyab.rayganshirazinejad@student.nhlstenden.com>
2024-10-07 11:57:11 +0200

    blinking

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,23 @@
 // put function declarations here:
 int myFunction(int, int);
 
-void setup() {
+void setup() 
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
+  digitalwrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int myFunction(int x, int y) 
+{
+  
 }
